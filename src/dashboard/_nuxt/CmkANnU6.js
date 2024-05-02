41c866b28f0d47e7aafbb927820205c0580448c4
allburov <allburov@gmail.com>
2024-05-02 13:57:58 +0700

    [plus] Add dashboard

---
+++ src/dashboard/_nuxt/CmkANnU6.js
@@ -0,0 +1,1 @@
+import r from"./CxwEbla3.js";import{P as t,t as o}from"./B72C839w.js";import"./iWUGzcQS.js";import"./BWSlOLt6.js";const i={__name:"AppSidebar",setup(e){return(p,a)=>(o(),t(r))}};export{i as default};
---
+++ src/dashboard/_nuxt/DLgWWicC.js
@@ -0,0 +1,1 @@
+import{ax as r}from"./B72C839w.js";var e=r();export{e as O};
