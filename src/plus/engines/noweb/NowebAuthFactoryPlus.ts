d080da9940f095536436b1bfc041c5747d222b12
devlikepro <waha@devlike.pro>
2024-05-27 18:45:00 +0700

    [plus] Fix bad creds.json
    Fix #347

--- src/plus/engines/noweb/NowebAuthFactoryPlus.ts
+++ src/plus/engines/noweb/NowebAuthFactoryPlus.ts
@@ -1,9 +1,10 @@
-import { Store } from 'whatsapp-web.js';
+import { join } from 'path';
 
 import { DataStore } from '../../../core/abc/DataStore';
 import { NowebAuthFactoryCore } from '../../../core/engines/noweb/NowebAuthFactoryCore';
 import { LocalStore } from '../../../core/storage/LocalStore';
 import { MongoStore } from '../../storage/MongoStore';
+import { makeSureJsonFile } from '../../utils/jsonutils';
 import { NoWebMongoDbAuth } from './NoWebMongoDbAuth';
 
 export class NowebAuthFactoryPlus extends NowebAuthFactoryCore {
@@ -18,5 +19,25 @@ export class NowebAuthFactoryPlus extends NowebAuthFactoryCore {
     const authStore = new NoWebMongoDbAuth(db);
     await authStore.init();
     return authStore.methods();
+  }
+
+  protected async buildLocalAuth(store: LocalStore, name: string) {
+    // Quick fix for
+    // https://github.com/devlikeapro/whatsapp-http-api/issues/347
+    // We can remove it after some time and the fix
+    // https://github.com/WhiskeySockets/Baileys/pull/824
+    await this.makeCredsJsonValid(store, name);
+    return super.buildLocalAuth(store, name);
+  }
+
+  private async makeCredsJsonValid(store: LocalStore, name: string) {
+    try {
+      await store.init(name);
+      const authFolder = store.getSessionDirectory(name);
+      const credsPath = join(authFolder, 'creds.json');
+      await makeSureJsonFile(credsPath);
+    } catch (error) {
+      console.error('Failed to fix "creds.json" file, continue...', error);
+    }
   }
 }
