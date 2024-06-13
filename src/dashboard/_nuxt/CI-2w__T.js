eb31fefa389c21bc6dc0200b6dcf10aa295002e5
devlikepro <waha@devlike.pro>
2024-06-13 15:57:19 +0700

    [plus] rm dashboard

--- src/dashboard/_nuxt/CI-2w__T.js
+++
@@ -1,1 +0,0 @@
-import{ax as r}from"./BBK-7gTl.js";var e=r();export{e as O};
--- src/dashboard/_nuxt/Cpo5BabE.js
+++
@@ -1,1 +0,0 @@
-import r from"./BuwlxSQ8.js";import{P as t,t as o}from"./BBK-7gTl.js";import"./DJClqKWP.js";import"./DR6YYPuf.js";const i={__name:"AppSidebar",setup(e){return(p,a)=>(o(),t(r))}};export{i as default};
