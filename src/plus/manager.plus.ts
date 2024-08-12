a13681123f4638903b4ef0ad6a2d2b03af19e5c1
devlikepro <waha@devlike.pro>
2024-08-12 10:15:43 +0700

    [PLUS] Add POST /api/sessions

--- src/plus/manager.plus.ts
+++ src/plus/manager.plus.ts
@@ -160,6 +160,10 @@ export class SessionManagerPlus extends SessionManager {
   //
   // API Methods
   //
+  async exists(name: string): Promise<boolean> {
+    return !!(await this.sessionConfigRepository.get(name));
+  }
+
   isRunning(name: string): boolean {
     return !!this.sessions[name];
   }
