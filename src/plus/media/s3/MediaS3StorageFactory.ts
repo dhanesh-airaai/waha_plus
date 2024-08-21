e0eaf9ee3335280063045c21c65b41b4fac5a867
devlikepro <waha@devlike.pro>
2024-08-21 15:30:30 +0700

    [PLUS] add MediaS3StorageConfig

--- src/plus/media/s3/MediaS3StorageFactory.ts
+++ src/plus/media/s3/MediaS3StorageFactory.ts
@@ -1,9 +1,9 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
-import { WhatsappConfigService } from '@waha/config.service';
 import { IMediaStorage } from '@waha/core/media/IMediaStorage';
 import { MediaStorageFactory } from '@waha/plus/media/MediaStorageFactory';
 import { MediaS3Storage } from '@waha/plus/media/s3/MediaS3Storage';
+import { MediaS3StorageConfig } from '@waha/plus/media/s3/MediaS3StorageConfig';
 import { Logger } from 'pino';
 
 @Injectable()
@@ -11,34 +11,10 @@ export class MediaS3StorageFactory extends MediaStorageFactory {
   private readonly s3client: S3Client;
   private readonly defaultBucket: string;
 
-  constructor(private config: WhatsappConfigService) {
+  constructor(private s3config: MediaS3StorageConfig) {
     super();
-
-    const region = this.config.get('WAHA_S3_REGION');
-    if (!region) {
-      throw new Error('WAHA_S3_REGION is required');
-    }
-    const accessKeyId = this.config.get('WAHA_S3_ACCESS_KEY_ID');
-    if (!accessKeyId) {
-      throw new Error('WAHA_S3_ACCESS_KEY_ID is required');
-    }
-    const secretAccessKey = this.config.get('WAHA_S3_SECRET_ACCESS_KEY');
-    if (!secretAccessKey) {
-      throw new Error('WAHA_S3_SECRET_ACCESS_KEY is required');
-    }
-    const endpoint = this.config.get('WAHA_S3_ENDPOINT', undefined);
-    this.s3client = new S3Client({
-      region: region,
-      credentials: {
-        accessKeyId: accessKeyId,
-        secretAccessKey: secretAccessKey,
-      },
-      endpoint: endpoint,
-    });
-    this.defaultBucket = this.config.get('WAHA_S3_BUCKET');
-    if (!this.defaultBucket) {
-      throw new Error('WAHA_S3_BUCKET is required');
-    }
+    this.s3client = new S3Client(this.s3config.s3ClientConfig);
+    this.defaultBucket = this.s3config.bucket;
   }
 
   build(logger: Logger): IMediaStorage {
--- src/plus/media/s3/media.s3.storage.module.ts
+++ src/plus/media/s3/media.s3.storage.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WhatsappConfigService } from '@waha/config.service';
 import { MediaStorageFactory } from '@waha/plus/media/MediaStorageFactory';
+import { MediaS3StorageConfig } from '@waha/plus/media/s3/MediaS3StorageConfig';
 import { MediaS3StorageFactory } from '@waha/plus/media/s3/MediaS3StorageFactory';
 
 @Module({
@@ -10,6 +11,7 @@ import { MediaS3StorageFactory } from '@waha/plus/media/s3/MediaS3StorageFactory
       useClass: MediaS3StorageFactory,
     },
     WhatsappConfigService,
+    MediaS3StorageConfig,
   ],
   exports: [MediaStorageFactory],
 })
