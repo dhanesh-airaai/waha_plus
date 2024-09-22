fb66a761b01118eedfa9524e3013ba0ff76c264d
devlikepro <waha@devlike.pro>
2024-09-22 17:50:53 +0700

    [PLUS] Use axios

--- src/plus/webhooks.plus.ts
+++ src/plus/webhooks.plus.ts
@@ -1,9 +1,12 @@
-import request = require('requestretry');
 import { LoggerBuilder } from '@waha/utils/logging';
+import { VERSION } from '@waha/version';
+import axios from 'axios';
+import { AxiosInstance } from 'axios';
+import axiosRetry from 'axios-retry';
 import * as crypto from 'crypto';
 
 import { WebhookSender } from '../core/abc/webhooks.abc';
-import { WebhookConductorCore } from '../core/webhooks.core';
+import { WebhookConductorCore, WebhookSenderCore } from '../core/webhooks.core';
 import { SECOND } from '../structures/enums.dto';
 import { WebhookConfig } from '../structures/webhooks.config.dto';
 
@@ -11,46 +14,46 @@ const DEFAULT_RETRY_DELAY_SECONDS = 2;
 const DEFAULT_RETRY_ATTEMPTS = 15;
 const DEFAULT_HMAC_ALGORITHM = 'sha512';
 
-export class WebhookSenderPlus extends WebhookSender {
-  private readonly attempts: number;
-  private readonly delayMs: number;
-  private readonly config: WebhookConfig;
-
-  constructor(loggerBuilder: LoggerBuilder, config: WebhookConfig) {
-    super(loggerBuilder, config);
-    this.attempts = config.retries?.attempts || DEFAULT_RETRY_ATTEMPTS;
-    const delaySeconds =
-      config.retries?.delaySeconds || DEFAULT_RETRY_DELAY_SECONDS;
-    this.delayMs = delaySeconds * SECOND;
-    this.config = config;
-  }
-
-  protected getHeaders(body) {
-    // Common Headers
+export class WebhookSenderPlus extends WebhookSenderCore {
+  protected buildAxiosInstance(): AxiosInstance {
+    // configure headers
+    const customHeaders = this.config.customHeaders || [];
     const headers = {
       'content-type': 'application/json',
+      'User-Agent': `WAHA/${VERSION.version}`,
     };
+    customHeaders.forEach((header) => {
+      headers[header.name] = header.value;
+    });
 
-    // Custom Headers
-    if (this.config.customHeaders) {
-      const customHeaders = {};
-      for (const header of this.config.customHeaders) {
-        customHeaders[header.name] = header.value;
-      }
-      Object.assign(headers, customHeaders);
-    }
+    // configure retry
+    const attempts = this.config.retries?.attempts ?? DEFAULT_RETRY_ATTEMPTS;
+    const delaySeconds =
+      this.config.retries?.delaySeconds ?? DEFAULT_RETRY_DELAY_SECONDS;
+    const delayMs = delaySeconds * SECOND;
 
+    const instance = axios.create({
+      headers: headers,
+      httpAgent: WebhookSenderCore.AGENTS.http,
+      httpsAgent: WebhookSenderCore.AGENTS.https,
+    });
+    axiosRetry(instance, {
+      retries: attempts,
+      retryDelay: (_) => delayMs,
+    });
+    return instance;
+  }
+
+  protected getHMACHeaders(body: string) {
     // HMAC
     const hmac = this.calculateHmac(body, DEFAULT_HMAC_ALGORITHM);
-    if (hmac) {
-      const hmacHeader = {
-        'X-Webhook-Hmac': hmac,
-        'X-Webhook-Hmac-Algorithm': DEFAULT_HMAC_ALGORITHM,
-      };
-      Object.assign(headers, hmacHeader);
+    if (!hmac) {
+      return {};
     }
-
-    return headers;
+    return {
+      'X-Webhook-Hmac': hmac,
+      'X-Webhook-Hmac-Algorithm': DEFAULT_HMAC_ALGORITHM,
+    };
   }
 
   private calculateHmac(body, algorithm) {
@@ -64,29 +67,10 @@ export class WebhookSenderPlus extends WebhookSender {
       .digest('hex');
   }
 
-  send(json) {
+  send(json: any) {
     const body = JSON.stringify(json);
-    const headers = this.getHeaders(body);
-    const postParams = {
-      body: body,
-      maxAttempts: this.attempts,
-      retryDelay: this.delayMs,
-      retryStrategy: request.RetryStrategies.HTTPOrNetworkError,
-      headers: headers,
-    };
-    this.logger.info(`Sending POST to ${this.url}...`);
-    this.logger.debug(`POST DATA: ${body}`);
-
-    request.post(this.url, postParams, (error, res, body) => {
-      if (error) {
-        this.logger.error(error);
-        return;
-      }
-      this.logger.info(
-        `POST request was sent with status code: ${res.statusCode}`,
-      );
-      this.logger.debug(`Response: ${body}`);
-    });
+    const headers = this.getHMACHeaders(body);
+    super.send(json, headers);
   }
 }
 
