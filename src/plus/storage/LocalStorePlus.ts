eab4a8e314b76df761b7582574a93cf1848cb36c
devlikepro <waha@devlike.pro>
2024-10-20 16:01:33 +0700

    [PLUS] Add WAHA_LOCAL_STORE_BASE_DIR
    fix #597

--- src/plus/storage/LocalStorePlus.ts
+++ src/plus/storage/LocalStorePlus.ts
@@ -3,7 +3,8 @@ import * as path from 'path';
 import { LocalStoreCore } from '../../core/storage/LocalStoreCore';
 
 export class LocalStorePlus extends LocalStoreCore {
-  protected readonly baseDirectory: string = './.sessions';
+  protected readonly baseDirectory: string =
+    process.env.WAHA_LOCAL_STORE_BASE_DIR || './.sessions';
 
   protected getDirectoryPath(name: string): string {
     return path.join(this.getEngineDirectory(), name);
