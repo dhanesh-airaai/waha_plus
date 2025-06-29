22dcd6260574b38c46021a82baf6890ba987decf
devlikepro <waha@devlike.pro>
2025-06-29 16:56:11 +0700

    [PLUS] Use KnexEngine (copied from PsqlKnexEngine.ts)

--- src/plus/storage/psql/PsqlKVRepository.ts
+++ src/plus/storage/psql/PsqlKVRepository.ts
@@ -1,6 +1,6 @@
+import { KnexEngine } from '@waha/core/storage/sql/KnexEngine';
 import { SqlKVRepository } from '@waha/core/storage/sql/SqlKVRepository';
 import { PsqlJsonQuery } from '@waha/plus/storage/psql/PsqlJsonQuery';
-import { PsqlKnexEngine } from '@waha/plus/storage/psql/PsqlKnexEngine';
 import Knex from 'knex';
 
 export class PsqlKVRepository<Entity> extends SqlKVRepository<Entity> {
@@ -8,7 +8,7 @@ export class PsqlKVRepository<Entity> extends SqlKVRepository<Entity> {
   protected jsonQuery = new PsqlJsonQuery();
 
   constructor(knex: Knex.Knex) {
-    const engine = new PsqlKnexEngine(knex);
+    const engine = new KnexEngine(knex);
     super(engine, knex);
   }
 }
