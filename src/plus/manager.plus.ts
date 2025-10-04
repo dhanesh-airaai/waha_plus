f0c229985e030916f6d2e444d924fd9bd7149f5c
devlikepro <waha@devlike.pro>
2025-10-04 16:07:08 +0700

    [PLUS] Fix NOWEB Apps on start
    fix #1397

--- src/plus/manager.plus.ts
+++ src/plus/manager.plus.ts
@@ -355,11 +355,15 @@ export class SessionManagerPlus
     webhook.configure(session, webhooks);
 
     // Apps
-    await this.configureApps(session);
+    await this.appsService.beforeSessionStart(session, this.store);
 
     // start session
     await session.start();
     logger.info('Session has been started.');
+
+    // Apps
+    await this.appsService.afterSessionStart(session, this.store);
+
     return {
       name: session.name,
       status: session.status,
